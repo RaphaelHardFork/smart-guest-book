973c58000c8393261277efd76d5cc5c91ee213eb
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-06-24 14:32:52 +0200

    creation

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-solhint')
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.4',
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: false,
+  },
+}
