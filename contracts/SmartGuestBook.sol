58be8dd3ba052fce9746e776d7499146359cbb3a
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-06-30 18:24:55 +0200

    not deployed yet

--- contracts/SmartGuestBook.sol
+++ contracts/SmartGuestBook.sol
@@ -6,8 +6,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract SmartGuestBook is ERC721Enumerable, ERC721URIStorage, AccessControl {
+    using Address for address payable;
     using Counters for Counters.Counter;
 
     struct Comment {
@@ -16,12 +18,25 @@ contract SmartGuestBook is ERC721Enumerable, ERC721URIStorage, AccessControl {
         uint256 tokenId;
         string cid;
     }
+
     bytes32 private constant _MODERATOR_ROLE = keccak256("MODERATOR_ROLE");
     Counters.Counter private _commentId;
     mapping(uint256 => Comment) private _comments;
+    mapping(uint256 => uint256) private _price; //OWNER PUT IN SALE HIS TOKEN
+    //mapping(address => mapping(uint256 => uint256)) private _offer; //BUYER MAKE AN OFFER
 
     event CommentLeaved(address indexed author, bytes32 indexed hashedComment, string cid, uint256 tokenId);
     event CommentDeleted(address indexed moderator, bytes32 indexed hashedComment, uint256 tokenId);
+
+    /* //BUYER MAKE AN OFFER
+    event OfferForComment(address indexed bider, address indexed tokenOwner, uint256 tokenId, uint256 price);
+    event OfferCancelled(address indexed bider, address indexed tokenOwner, uint256 tokenId, uint256 price);
+    */
+
+    //OWNER PUT IN SALE HIS TOKEN
+    event CommentInSale(address indexed seller, uint256 tokenId, uint256 price);
+    event CommentRemovedFromSale(address indexed seller, uint256 tokenId);
+    event CommentBought(address indexed seller, address indexed buyer, uint256 tokenId, uint256 price);
 
     constructor() ERC721("Comment", "COM") {
         _setupRole(_MODERATOR_ROLE, msg.sender);
@@ -42,6 +57,61 @@ contract SmartGuestBook is ERC721Enumerable, ERC721URIStorage, AccessControl {
         bytes32 hashedComment = dataOf(tokenId).hashedComment;
         _burn(tokenId);
         emit CommentDeleted(msg.sender, hashedComment, tokenId);
+        return true;
+    }
+
+    /* //BUYER MAKE AN OFFER
+    function makeOffer(uint256 tokenId) public payable returns (uint256) {
+        _offer[msg.sender][tokenId] = msg.value;
+        emit OfferForComment(msg.sender, ownerOf(tokenId), tokenId, msg.value);
+        return msg.value;
+    }
+
+    function cancelOffer(uint256 tokenId) public returns (bool) {
+        require(_offer[msg.sender][tokenId] != 0, "SmartGuestBook: you don't made offer for this token.");
+        uint256 offer = _offer[msg.sender][tokenId];
+        _offer[msg.sender][tokenId] = 0;
+        payable(msg.sender).sendValue(offer);
+        emit OfferCancelled(msg.sender, ownerOf(tokenId), tokenId, offer);
+        return true;
+    }
+
+    function acceptOffer(uint256 tokenId, address bider) public returns (bool) {
+        uint256 offer = _offer[bider][tokenId];
+        _offer[msg.sender][tokenId] = 0;
+        transferFrom(msg.sender, bider, tokenId);
+        payable(msg.sender).sendValue(offer);
+        return true;
+    }
+
+    function offerForFrom(uint256 tokenId, address bider) public view returns (uint256) {
+        return _offer[bider][tokenId];
+    }
+    */
+
+    //OWNER PUT IN SALE HIS TOKEN
+    function sellComment(uint256 tokenId, uint256 price) public returns (uint256) {
+        approve(address(this), tokenId);
+        _price[tokenId] = price;
+        emit CommentInSale(msg.sender, tokenId, price);
+        return price;
+    }
+
+    function removeFromSale(uint256 tokenId) public returns (bool) {
+        approve(address(0), tokenId);
+        _price[tokenId] = 0;
+        emit CommentRemovedFromSale(msg.sender, tokenId);
+        return true;
+    }
+
+    function buyComment(uint256 tokenId) public payable returns (bool) {
+        require(msg.value >= _price[tokenId], "SmartGuestBook: value too low to buy this token.");
+        uint256 rest = msg.value - _price[tokenId];
+        address owner = ownerOf(tokenId);
+        payable(owner).sendValue(_price[tokenId]);
+        _transfer(ownerOf(tokenId), msg.sender, tokenId);
+        payable(msg.sender).sendValue(rest);
+        emit CommentBought(owner, msg.sender, tokenId, _price[tokenId]);
         return true;
     }
 
--- test/SmartGuestBook-test.js
+++ test/SmartGuestBook-test.js
@@ -3,14 +3,16 @@ const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
 describe('SmartGuestBook', function () {
-  let SmartGuestBook, smartGuestBook, dev, author1
+  let SmartGuestBook, smartGuestBook, dev, author1, buyer
   const NAME = 'Comment'
   const SYMBOL = 'COM'
   const HASHED_COMMENT = ethers.utils.id('salut ton site est genial')
   const CID = 'QmZg7MTxjfgV54H2ZHZreqEnSAbcxaepLGYAaWAN6Ershh'
+  const PRICE = ethers.utils.parseEther('0.1')
+  const ZERO_ADDRESS = ethers.constants.AddressZero
 
   beforeEach(async function () {
-    ;[dev, author1] = await ethers.getSigners()
+    ;[dev, author1, buyer] = await ethers.getSigners()
     SmartGuestBook = await ethers.getContractFactory('SmartGuestBook')
     smartGuestBook = await SmartGuestBook.connect(dev).deploy()
     await smartGuestBook.deployed()
@@ -71,6 +73,64 @@ describe('SmartGuestBook', function () {
       expect(await smartGuestBook.connect(dev).deleteComment(1))
         .to.emit(smartGuestBook, 'CommentDeleted')
         .withArgs(dev.address, hashedComment.hashedComment, 1)
+    })
+  })
+
+  describe('Selling tokens', function () {
+    let sellCall
+    beforeEach(async function () {
+      await smartGuestBook.connect(author1).comment(HASHED_COMMENT, CID)
+      sellCall = await smartGuestBook.connect(author1).sellComment(1, PRICE)
+    })
+
+    it('should approve contract for this token', async function () {
+      expect(await smartGuestBook.getApproved(1)).to.equal(
+        smartGuestBook.address
+      )
+    })
+
+    it('should emit a CommentInSale event', async function () {
+      expect(sellCall)
+        .to.emit(smartGuestBook, 'CommentInSale')
+        .withArgs(author1.address, 1, PRICE)
+    })
+
+    it('should can remove token from sale', async function () {
+      await smartGuestBook.connect(author1).removeFromSale(1)
+      expect(await smartGuestBook.getApproved(1)).to.equal(ZERO_ADDRESS)
+    })
+
+    it('should emit a CommentRemovedFromSale event when token removed from sale', async function () {
+      expect(await smartGuestBook.connect(author1).removeFromSale(1))
+        .to.emit(smartGuestBook, 'CommentRemovedFromSale')
+        .withArgs(author1.address, 1)
+    })
+
+    it('should buy the token', async function () {
+      await smartGuestBook.connect(buyer).buyComment(1, { value: PRICE })
+      expect(await smartGuestBook.ownerOf(1)).to.equal(buyer.address)
+    })
+
+    it('should emit a CommentBought after the buy', async function () {
+      expect(
+        await smartGuestBook.connect(buyer).buyComment(1, { value: PRICE })
+      )
+        .to.emit(smartGuestBook, 'CommentBought')
+        .withArgs(author1.address, buyer.address, 1, PRICE)
+    })
+
+    it('should revert if the value send is to low', async function () {
+      await expect(
+        smartGuestBook.connect(buyer).buyComment(1, { value: PRICE.div(2) })
+      ).to.be.revertedWith('SmartGuestBook: value too low to buy this token.')
+    })
+
+    it('should refund if the value for the token is too high', async function () {
+      expect(
+        await smartGuestBook
+          .connect(buyer)
+          .buyComment(1, { value: PRICE.mul(3) })
+      ).to.changeEtherBalance(buyer, PRICE.mul(-1))
     })
   })
 })
